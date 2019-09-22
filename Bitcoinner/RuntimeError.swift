71956b7a137fde482306db2ed24405061f158230
Matheus Fróes Marques <matheusfroesmarques@gmail.com>
2019-09-22 14:32:37 -0300

    Implements custom Swift.Error

---
+++ Bitcoinner/RuntimeError.swift
@@ -0,0 +1,21 @@
+//
+//  RuntimeError.swift
+//  Bitcoinner
+//
+//  Created by Matheus Fróes on 22/09/19.
+//  Copyright © 2019 Matheus Fróes. All rights reserved.
+//
+
+import Foundation
+
+struct RuntimeError: Error {
+    let message: String
+    
+    init(_ message: String) {
+        self.message = message
+    }
+    
+    public var localizedDescription: String {
+        return message
+    }
+}
